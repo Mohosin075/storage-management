d198f09768171b741b8626b3cff0a6b516e10564
Mohosin <mohosinali075@gmail.com>
2025-05-25 22:22:35 +0600

    add routes.ts and modify routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import authRouter from './app/modules/auth/auth.rotue';
-import userRouter from './app/modules/user/user.route';
+import router from './app/routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
+app.use('/api', router);
 
 export default app;
---
+++ src/app/routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import userRouter from './modules/user/user.route';
+import authRouter from './modules/auth/auth.route';
+
+const router = express.Router();
+
+router.use('/users', userRouter);
+
+router.use('/auth', authRouter);
+
+export default router;
