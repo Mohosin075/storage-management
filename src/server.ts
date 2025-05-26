b688cecf0b8b9c339950e7659c042b1221f1c197
Mohosin <mohosinali075@gmail.com>
2025-05-26 16:00:20 +0600

    root / url added

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import app from './app';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const uri = process.env.DATABASE_URL as string;
 
-mongoose.connect(uri)
+mongoose
+  .connect(uri)
   .then(() => {
-    console.log('MongoDB connected');
+    app.get("/", (req, res) => {
+      res.send("Welcome to the Storage Management System!");
+    });
+    console.log("MongoDB connected");
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch(err => console.error('DB Connection Error:', err));
+  .catch((err) => console.error("DB Connection Error:", err));
